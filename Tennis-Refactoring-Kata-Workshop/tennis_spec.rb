bd2f84f2d4ba09e196ec1fa24e9f8b5605ae9e27
Tommaso Torti <torti.tommaso@gmail.com>
2022-01-20 16:46:33 +0100

    rspec example

--- Tennis-Refactoring-Kata-Workshop/tennis_spec.rb
+++ Tennis-Refactoring-Kata-Workshop/tennis_spec.rb
@@ -2,8 +2,36 @@ require_relative 'tennis'
 
 describe TennisGame1 do
 
-    it "xxx" do
-      expect(1).to eq(1)
+  let(:player1) do
+    'player1'
+  end
+
+  let(:player2) do
+    'player2'
+  end
+
+  let(:game) do
+   TennisGame1.new(player1, player2)
+  end
+
+  it "deuce" do
+    game.won_point('player1')
+    game.won_point('player2')
+    expect(game.score).to eq('Fifteen-All')
+  end
+
+  it "ongoing" do
+    game.won_point('player1')
+    game.won_point('player1')
+    game.won_point('player2')
+    expect(game.score).to eq('Thirty-Fifteen')
+  end
+
+  it "player1 wins" do
+    (1..4).each do
+      game.won_point('player1')
     end
+    expect(game.score).to eq('Win for player1')
+  end
 
 end
