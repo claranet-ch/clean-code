8a05352917b1f1f1352a10e22538a1fc072a00db
Pasquale Panuccio <pasquale.panuccio@claranet.com>
2022-02-07 15:38:15 +0100

    Added tests

--- FizzBuzz/fizz_buzz.rb
+++ FizzBuzz/fizz_buzz.rb
@@ -1,6 +1,21 @@
 class FizzBuzz
 
   def say(number)
+    if is_multiple_of(number, 3) and is_multiple_of(number,5)
+      return "FizzBuzz"
+    end
+    if is_multiple_of(number, 3)
+      return "Fizz"
+    end
+    if is_multiple_of(number, 5)
+      return "Buzz"
+    end
     number.to_s
+  end
+
+  private
+
+  def is_multiple_of(number, multiple)
+    number % multiple == 0
   end
 end
--- FizzBuzz/fizzbuzz_spec.rb
+++ FizzBuzz/fizzbuzz_spec.rb
@@ -1,12 +1,25 @@
 require_relative 'fizz_buzz'
 require 'rspec'
 
-describe "FizzBuzz" do
+describe "FizzBuzz Should" do
 
   let(:fizzBuzz) { FizzBuzz.new }
 
-  it 'plain numbers' do
+  it 'return string version of number' do
     expect(fizzBuzz.say(1)).to eq("1")
+    expect(fizzBuzz.say(2)).to eq("2")
+  end
+
+  it 'return Fizz on multiple of three' do
+    expect(fizzBuzz.say(3)).to eq("Fizz")
+  end
+
+  it 'return Buzz on multiple of five' do
+    expect(fizzBuzz.say(5)).to eq("Buzz")
+  end
+
+  it 'return FizzBuzz on multiple of three and five' do
+    expect(fizzBuzz.say(15)).to eq("FizzBuzz")
   end
 
 end
