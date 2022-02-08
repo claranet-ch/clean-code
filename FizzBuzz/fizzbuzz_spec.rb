1d51bdd52065939d7f45a8ca603bff2a3c29458c
Pasquale Panuccio <pasquale.panuccio@claranet.com>
2022-02-08 10:13:41 +0100

    Refactoring

--- FizzBuzz/fizzbuzz_spec.rb
+++ FizzBuzz/fizzbuzz_spec.rb
@@ -3,15 +3,15 @@ require 'rspec'
 
 describe "FizzBuzz" do
 
+  let(:fizzbuzz){FizzBuzz.new}
+
   it 'numbers not multiple of 3 or 5 are plain string' do
-    fb = FizzBuzz.new
-    expect(fb.say(1)).to eq("1")
+    expect(fizzbuzz.say(1)).to eq("1")
   end
 
   it 'for numbers multiple of three I should say "Fizz" ' do
-    fb = FizzBuzz.new
-    expect(fb.say(3)).to eq("Fizz")
-    expect(fb.say(6)).to eq("Fizz")
+    expect(fizzbuzz.say(3)).to eq("Fizz")
+    expect(fizzbuzz.say(6)).to eq("Fizz")
   end
 
 
