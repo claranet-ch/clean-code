370bb0b4bece3f161bdee44cf6d6ddf6b585ce4b
Pasquale Panuccio <pasquale.panuccio@claranet.com>
2022-02-08 10:34:42 +0100

    Return Buzz for multiple of 5

--- FizzBuzz/fizzbuzz.rb
+++ FizzBuzz/fizzbuzz.rb
@@ -1,10 +1,20 @@
 class FizzBuzz
 
   def say number
-    if number % 3 == 0
+    if is_multiple_of(number, 3)
       return "Fizz"
+    end
+    if is_multiple_of(number, 5)
+      return "Buzz"
     end
     number.to_s
   end
+
+  private
+
+  def is_multiple_of(number, multiple)
+    number % multiple == 0
+  end
+
 
 end
--- FizzBuzz/fizzbuzz_spec.rb
+++ FizzBuzz/fizzbuzz_spec.rb
@@ -5,13 +5,18 @@ describe "FizzBuzz" do
 
   let(:fizzbuzz){FizzBuzz.new}
 
-  it 'numbers not multiple of 3 or 5 are plain string' do
+  it 'say number as default' do
     expect(fizzbuzz.say(1)).to eq("1")
   end
 
   it 'for numbers multiple of three I should say "Fizz" ' do
     expect(fizzbuzz.say(3)).to eq("Fizz")
     expect(fizzbuzz.say(6)).to eq("Fizz")
+  end
+
+  it 'for numbers multiple of five I should say "Buzz" ' do
+    expect(fizzbuzz.say(5)).to eq("Buzz")
+    expect(fizzbuzz.say(10)).to eq("Buzz")
   end
 
 
